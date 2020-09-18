7ead5e088eb0b2ec724eec47efb5f60a5e9bfa27
Gzoref <gzoref@gmail.com>
2020-09-18 14:39:41 -0400

    Closure with a loop

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,16 @@
+function createClassRoom(numbersOfStudents) {
+  function studentSeat(seat) {
+    return function () {
+      return seat;
+    }
+  }
+
+  let students = [];
+
+  for (let index = 0; index < numbersOfStudents; index++) {
+    students.push(studentSeat(index + 1));
+  }
+  return students;
+}
+
+let classRoom = createClassRoom(10);
