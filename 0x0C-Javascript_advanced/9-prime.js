a6cc3d7a92d5286e16d559c371a0345f213bcbc5
Gzoref <gzoref@gmail.com>
2020-09-19 17:54:11 -0400

    Remove loop

--- 0x0C-Javascript_advanced/9-prime.js
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -1,23 +1,23 @@
 function countPrimeNumbers() {
-
   let numberOfPrimes = 0;
 
   for (let index = 2; index <= 100; index++) {
-	  if(100 % index == 0){
-		  numberOfPrimes++;
-	  }
+    if (100 % index == 0) {
+      numberOfPrimes++;
+    }
   }
+  console.log(numberOfPrimes);
   return numberOfPrimes;
 }
 
 const time0 = performance.now();
 
-for (let index = 0; index < 100; index++) {
-	countPrimeNumbers();
-}
+countPrimeNumbers();
 
 const time1 = performance.now();
 
 const time = time1 - time0;
 
-console.log(`Execution time of printing countPrimeNumbers was ${time} milliseconds`);
+console.log(
+  `Execution time of printing countPrimeNumbers was ${time} milliseconds`
+);
