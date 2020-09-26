f1f4486b7e25c337a9718f6103b5a1a3b7dd3da1
Gzoref <gzoref@gmail.com>
2020-09-26 19:57:16 -0400

    Fix order of function calls

--- 0x0C-Javascript_advanced/1-nested_functions.js
+++ 0x0C-Javascript_advanced/1-nested_functions.js
@@ -3,20 +3,16 @@ const globalVariable = "Welcome";
 function outer() {
   alert(globalVariable);
   const course = "Holberton";
-  inner();
 
   function inner() {
     alert(`${globalVariable} ${course}`);
     const exclamation = "!";
-    inception();
 
     function inception() {
       alert(`${globalVariable} ${course} ${exclamation}`);
-
-      inner();
-      outer();
     }
+    inception();
   }
+  inner();
 }
-
 outer();
